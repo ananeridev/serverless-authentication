f6ecdbfddb3cc4aebec3239978fb7b2204c63475
Ana Neri <ananeri32@hotmail.com>
2023-11-21 23:25:38 -0300

    refactor with jwt apis
    Signed-off-by: Ana Neri <ananeri32@hotmail.com>

--- handlers/handler.js
+++ handlers/handler.js
@@ -1,33 +1,45 @@
-const mongoose = require("mongoose");
+// handler.js
 const jwt = require("jsonwebtoken");
 const User = require("./models/User");
-
-mongoose.connect("", { useNewUrlParser: true, useUnifiedTopology: true });
-
-const create = async (event) => {
-  const data = JSON.parse(event.body);
-  const user = new User(data);
-  await user.save();
-  return { statusCode: 200, body: JSON.stringify(user) };
+const SECRET_KEY = "secret_key";
+const login = async (event) => {
+  const { email, password } = JSON.parse(event.body);
+  const user = await User.findOne({ email, password }); // only for test purpose
+  if (!user) {
+    return {
+      statusCode: 401,
+      body: JSON.stringify({ message: "Invalid credentials" }),
+    };
+  }
+  const token = jwt.sign({ id: user._id }, SECRET_KEY);
+  return { statusCode: 200, body: JSON.stringify({ token }) };
 };
 
-const read = async (event) => {
-  const { id } = event.pathParameters;
-  const user = await User.findById(id);
-  return { statusCode: 200, body: JSON.stringify(user) };
+const authenticate = (handler) => async (event) => {
+  const { authorization } = event.headers;
+  if (!authorization) {
+    return {
+      statusCode: 401,
+      body: JSON.stringify({ message: "No token provided" }),
+    };
+  }
+  try {
+    const token = authorization.replace("Bearer ", "");
+    const { id } = jwt.verify(token, SECRET_KEY);
+    const user = await User.findById(id);
+    if (!user) {
+      return {
+        statusCode: 401,
+        body: JSON.stringify({ message: "Invalid token" }),
+      };
+    }
+    return handler(event);
+  } catch (err) {
+    return {
+      statusCode: 401,
+      body: JSON.stringify({ message: "Invalid token" }),
+    };
+  }
 };
 
-const update = async (event) => {
-  const { id } = event.pathParameters;
-  const data = JSON.parse(event.body);
-  const user = await User.findByIdAndUpdate(id, data, { new: true });
-  return { statusCode: 200, body: JSON.stringify(user) };
-};
-
-const remove = async (event) => {
-  const { id } = event.pathParameters;
-  await User.findByIdAndDelete(id);
-  return { statusCode: 200, body: JSON.stringify({ message: "User deleted" }) };
-};
-
-module.exports = { create, read, update, remove };
+module.exports = { login, authenticate };
